327437e7f11f317ebe254a0f43b47393eed7b54b
Matt Wright <mttwright@gmail.com>
2019-12-15 22:54:03 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,41 @@
+class Dog
+  
+  attr_accessor :id, :name, :breed
+  
+  def initialize(id: nil, name:, breed:)
+    self.id = id
+    self.name = name
+    self.breed = breed
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      );
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs;
+    SQL
+  
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+  end
+  
+  
+  
+  
+end
