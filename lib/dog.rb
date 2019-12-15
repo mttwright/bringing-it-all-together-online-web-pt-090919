bbcfc8e98367ae566ed1eee09599d871aeea084e
Matt Wright <mttwright@gmail.com>
2019-12-15 23:23:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,7 +64,6 @@ class Dog
     
       DB[:conn].execute(sql, self.name, self.breed)
       self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs;")[0][0]
-      self
     end
   end
   
